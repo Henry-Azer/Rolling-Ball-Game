b51f692994c74732b6aef0a00f8db0d3b2952134
Henry-Azer <henryazer@outlook.com>
2022-04-01 18:29:18 +0200

    Complete first sprint

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+
+{
+    public float speed;
+    private Rigidbody rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rigidBody.AddForce(movement*speed);
+    }
+
+    private void OnTriggerEnter(Collider collider) {
+        if(collider.gameObject.CompareTag("coin")) {
+            collider.gameObject.SetActive(false);
+        }
+    }
+}
